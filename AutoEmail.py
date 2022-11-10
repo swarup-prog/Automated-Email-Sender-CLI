9627a69a13f8bc0cf734b5481140fe1c886a9a1b
swarup-prog <lswarup1234@gmail.com>
2022-11-10 13:37:19 +0545

    Final Update

---
+++ AutoEmail.py
@@ -0,0 +1,30 @@
+import smtplib
+import pwinput as pwd
+import ssl
+from email.message import EmailMessage
+import pandas as pd
+
+# Get details of email sender and receiver
+email_sender = input('Email: ')
+email_password = pwd.pwinput(prompt='Password: ', mask='*')
+email_receiver = 'receiverEmail@gmail.com'
+
+# Set the subject and body of the email
+subject = 'Subject'
+message = """
+Message
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['Subject'] = subject
+em.set_content(message)
+
+# Add SSL (layer of security)
+context = ssl.create_default_context()
+
+# Log in and send the email
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Multiple-Email-Sender-Puthon-CLI
+# Python-Email-Sender-CLI
+This is a python program that sends emails using smtplib library.
+
+Note:
+1. To send the email, make sure your E-mail has 2-factor Authentication on.
+2. DO NOT use your original password. Instead generate password from myaccounts.google.com/u/1/apppasswords
+3. If you completed the above steps, you are good to go.
